0e704182c6862e318dd0787b52cf02bafe76c587
devadi <devadi@192.168.0.14>
2019-01-18 12:00:59 -0800

    Sindhu:1/18-Commiting a new java file.

---
+++ TestJenkins/src/test/java/com/test/java/TestName.java
@@ -0,0 +1,15 @@
+package com.test.java;
+
+public class TestName {
+	
+	
+	public static void main(String[ ] args){
+		int i;
+		
+		
+		for(i=0; i<=5; i++){
+		System.out.println("Sindhu");
+		}
+	}
+}
+
